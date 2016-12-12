d2558b02150c33ff090372be6a2cb80f453c0d75
Jesse <lenjoyshow@gmail.com>
2016-12-12 16:01:24 +0800

    ADD: patch gradle plugin

---
+++ patcher-build/patch-gradle-plugin/src/main/groovy/cn/jesse/patcher/build/gradle/PatcherPlugin.groovy
@@ -0,0 +1,12 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ *
+ */
+public class TestPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+
+    }
+}
